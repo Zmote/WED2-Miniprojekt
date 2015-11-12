4de33be1c4bab39930b96056f63dbaa75491aef0
Zmote <shodaime.hikarikage@hotmail.com>
2015-11-12 23:27:06 +0100

    Events first steps

--- webapp/source/classes/controllers/EventController.js
+++ webapp/source/classes/controllers/EventController.js
@@ -1,24 +1,9 @@
-
-/**
- * Created by Dogan on 12.11.15.
- */
 define([], function () {
-
      var EventController = function ($scope,$http){
-
-
-
              $http.get("/api/events").then(function(data){
-                 $scope.events = data;
+                 this.scope = $scope;
+                 this.scope.events = data.data.events;
              })
-
-
-
-
-
-     }
-
+     };
     return EventController;
-
-
 });
